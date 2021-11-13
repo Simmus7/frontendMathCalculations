338ff08458f08bcb85caad045fbcd5869b6b1cad
Simmus7 <64326113+Simmus7@users.noreply.github.com>
2021-11-13 00:05:18 -0500

    creación vistas y funcionamiento de fibonacci

---
+++ src/pages/Fibonacci.js
@@ -0,0 +1,93 @@
+import React, { Component, useState } from 'react';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import axios from 'axios';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { Dropdown } from 'primereact/dropdown';
+
+export default class Fibonacci extends Component{
+    constructor(props){
+        super(props)
+
+        
+        this.onChangeN = this.onChangeN.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.didGetResponse = this.didGetResponse.bind(this);
+        
+        this.state = {
+            n : 0,
+            fibonacciSequence : "",
+            didSubmit:false,
+        }
+    }
+
+    onChangeN(e) {
+        this.setState({
+          n: e.target.value
+        })
+        console.log("cambió el state")
+    }
+
+    onSubmit(e) {
+        console.log(this.state.n)
+        const toGet = {
+            n : this.state.n
+          }
+          axios
+          .post('http://localhost:5000/fibonacci', toGet)
+          .then(
+              
+              res => {
+                  console.log(res.data);
+                  if (res.data == "Please enter a valid number"){
+                    this.setState({
+                        didSubmit : false
+                      })
+                  }
+                  else {
+                    console.log("recibió respuesta correcta")
+                    this.setState({
+                        fibonacciSequence: res.data.n,
+                        didSubmit : true
+                      })
+                  }
+              }
+          )
+    }
+
+    didGetResponse() {
+        if (this.state.didSubmit == true){
+            return (<div>
+                        <div>The first {this.state.n} numbers of the fibonacci sequence are: </div>
+                        <br/>
+                        <div>{this.state.fibonacciSequence}</div>
+                    </div>
+                    )
+            
+        }
+        else {
+            return "Please enter a valid number"
+        }
+        
+
+    }
+
+    render() {
+        return (
+            <div className="card">
+            <h5>Fibonacci</h5>
+            <form onSubmit={this.onSubmit}> 
+                <div className="formgroup-inline">
+                    <div className="field">
+                        <InputText id="firstname1" type="text" placeholder="N" onChange={this.onChangeN} />
+                    </div>
+                        <Button label="Get sequence!" type="submit"></Button>
+                </div>
+                
+            </form>
+            {this.didGetResponse()}
+            
+        </div>
+        );
+    } 
+}
