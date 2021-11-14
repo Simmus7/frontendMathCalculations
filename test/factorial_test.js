a388031a1bfdb3a8446eefbcff6b3974939231e4
Simmus7 <64326113+Simmus7@users.noreply.github.com>
2021-11-14 12:56:51 -0500

    creacion prueba factorial

--- test/factorial_test.js
+++ test/factorial_test.js
@@ -1,6 +1,6 @@
 require('chromedriver')
 const { Builder, By, Key, until } = require('selenium-webdriver')
-const assert = require('assert')
+
 
 describe('factorial', function() {
   this.timeout(30000)
@@ -19,7 +19,6 @@ describe('factorial', function() {
     await driver.findElement(By.id("firstname1")).sendKeys("4")
     await driver.findElement(By.css(".p-button-label:nth-child(1)")).click()
     await driver.wait(until.elementTextIs(await driver.findElement(By.id("result")), '24'), 10000)
-    
     await driver.close()
   })
 })
